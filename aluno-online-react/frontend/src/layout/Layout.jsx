36be57aaf1bdc04deab17b23d42d0d58449d6850
Hiarley007 <hiarleymorais82@gmail.com>
2026-05-28 18:49:57 -0300

    feat: adicionado arquivo forms e ajustando a estlização

--- aluno-online-react/frontend/src/layout/Layout.jsx
+++ aluno-online-react/frontend/src/layout/Layout.jsx
@@ -1,18 +1,15 @@
 import { Outlet } from "react-router";
-import Menu from "../components/Menu"
+import Menu from "../components/Menu";
 import Sidebar from "../components/Sidebar";
 
-function Layout () {
-    return (
-        <>
-    
-            <Sidebar />
-        
-    
-            <Outlet/>
-       
-        </>
-    )
+function Layout() {
+  return (
+    <>
+      <Sidebar />
+
+      <Outlet />
+    </>
+  );
 }
 
-export default Layout; 
+export default Layout;
